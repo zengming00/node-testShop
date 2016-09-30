5b71a3219aee96511a04bcea45dec8f399ef0022
zengming <243786753@qq.com>
2016-09-30 13:52:12 +0800

    0

---
+++ models/model.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+
+var uri = 'mongodb://zengming:zengming@ds047146.mlab.com:47146/zengming';
+
+mongoose.connect(uri);
+var Schema = mongoose.Schema;
+
+var demoSchema = new Schema({
+    uid: String,
+    title: String,
+    content: String,
+    createTime: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+exports = module.exports = mongoose.model('demo', demoSchema);
