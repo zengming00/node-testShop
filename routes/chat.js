ba27ef314759d3af635f9d7d437d264d801f0319
zengming <243786753@qq.com>
2016-10-05 10:29:50 +0800

    .

--- routes/chat.js
+++ routes/chat.js
@@ -5,7 +5,10 @@ var router = express.Router();
 
 // 首页
 router.get('/', function(req, res, next) {
-	var host = req.protocol + '://' + req.hostname;  //+':'+req.app.get('port');
+	var host = 'http://' + req.hostname;  //+':'+req.app.get('port');
+    if(host === 'http://localhost'){
+        host = 'http://localhost:3000';
+    }
     res.render('chat/index',{host: host});
 });
 
