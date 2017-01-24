9237ddee86129601c8d0dc2dba443d558ea1ab64
zengming <243786753@qq.com>
2017-01-24 11:13:39 +0800

    修改.gitignore

---
+++ routes/test/test.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+var Page = require('../../lib/Page');
+var Comm = require('../../lib/Common');
+
+// 首页
+router.get('/', function(req, res, next) {
+    var $page = new Page(req, 51, 10);
+    var data = {
+        limit: "limit {" + $page.firstRow + "," + $page.listRows + "}",
+	    page: $page.show()
+    };
+    res.render('Page/index',data);
+});
+
+router.get('/pay', function(req, res){
+    res.send(Comm.getPayForm(1234111, 0.53));
+});
+
+
+module.exports = router;
